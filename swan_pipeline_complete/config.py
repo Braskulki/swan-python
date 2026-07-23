5e624ad68223fefb18a1f633f2a7ecc492c0d93f
Yuri Simões Braskulki <yuribraskulki@hotmail.com>
2026-07-23 10:40:03 -0300

    feat: processed folder

--- swan_pipeline_complete/config.py
+++ swan_pipeline_complete/config.py
@@ -15,30 +15,35 @@ BASE_DIR = Path(__file__).resolve().parent
 UNSTRUCTURED_DIR = BASE_DIR / "data" / "unstructured_research"
 UNSTRUCTURED_CASE_DIR = UNSTRUCTURED_DIR
 UNSTRUCTURED_PUBLICATION_DIR = UNSTRUCTURED_DIR / "publication"
-ERA5_YEAR = "2023"
-ERA5_MONTH = "01"
+ERA5_YEAR = "2017"
+ERA5_MONTH = "04"
 ERA5_DAYS = [
-    "01",
-    "02",
-    "03",
+    "04",
+    "05",
+    "06",
 ]
 
 ERA5_TIMES = [
-    "00:00",
-    "06:00",
-    "12:00",
-    "18:00",
+    "00:00","01:00","02:00","03:00",
+    "04:00","05:00","06:00","07:00",
+    "08:00","09:00","10:00","11:00",
+    "12:00","13:00","14:00","15:00",
+    "16:00","17:00","18:00","19:00",
+    "20:00","21:00","22:00","23:00",
 ]
 ## LON_MIN = -48.12
 ## LON_MAX = -44.65
 ## LAT_MIN = -25.36
 ## LAT_MAX = -23.18
-LON_MIN = -36.80
-LON_MAX = -34.80
+LON_MIN = -48.20
+LON_MAX = -44.50
 
-LAT_MIN = -10.70
-LAT_MAX = -8.30
+LAT_MIN = -26.00
+LAT_MAX = -23.00
+
 ERA5_MARGIN_DEGREES = 0.30
+
+
 GRID_DX = 0.025
 GRID_DY = 0.025
 GEBCO_STEP = 9
